4620fb0a8f30df248daa5fcb6311130a7e93c1cd
arjunzzoffside <amanoj23comp@student.mes.ac.in>
2026-07-14 01:03:30 +0530

    Initial commit

---
+++ patterns/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // ios::sync_with_stdio(false);
+    // cin.tie(nullptr);
+        int n;
+    cout<<"enter number:";
+
+    cin>>n;
+    
+    for(int i=0;i<n;i++){
+        for (int j=0;j<n;j++){
+            cout<<"*";
+        }cout<<endl;
+    }
+    return 0;
+}
+
